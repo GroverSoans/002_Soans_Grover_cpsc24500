e3172e07cbec499c3e736ec903e8ad06e198f4bf
Grover Soans <soansg3445@gmail.com>
2022-09-14 09:37:39 -0500

    Homework 1

--- src/Flooring/Flooring.java
+++ src/Flooring/Flooring.java
@@ -7,13 +7,14 @@ public class Flooring
         double areaOfLaminateIn = 144;
         double priceOfPack = 24.99;
         double totalAreaFt = 275;
-        
+
         double totalAreaIn = totalAreaFt * 12;
+
         double amountOfBoards = totalAreaIn/areaOfLaminateIn*1.25;
         double packsOfFlooring = Math.ceil(amountOfBoards/8);
         double costOfFlooring = packsOfFlooring * priceOfPack;
+        
         System.out.printf("Packs of Flooring needed:%.0f, Total Cost of Flooring:$%.2f", packsOfFlooring, costOfFlooring);
-
     }
     
 }
