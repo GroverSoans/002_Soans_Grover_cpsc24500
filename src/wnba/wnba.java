f6fe0f7f843013e179939381eee2300c1e50772b
Grover Soans <soansg3445@gmail.com>
2022-10-17 17:14:09 -0500

    west & east print

--- src/wnba/wnba.java
+++ src/wnba/wnba.java
@@ -1,6 +1,7 @@
 package wnba;
 import java.util.Scanner;
 import java.io.File;
+import java.util.ArrayList;
 ///Users/groversoans/Downloads/wnba.txt
 public class wnba {
     public static void heading(){
@@ -16,51 +17,102 @@ public class wnba {
         System.out.println("4. Exit");
         System.out.print("Enter the number of your choice: ");
     }
-    public static void printResults(){
+    public static void printResults(String[] conference){
+        String[] parts = new String[2];
+        String team;
+        int wins,loss,leaderW,leaderL;
+        double gb,pct;
 
-    } 
+        parts = conference[0].split("\t");
+        leaderW = Integer.parseInt(parts[1]);
+        leaderL = Integer.parseInt(parts[2]);
+
+        System.out.printf("%-20s %16s %8s %8s  %6s\n","Team Name", "Wins", "Losses", "PCT", "GB");
+
+        for (int i = 0; i < conference.length;i++){
+            parts = conference[i].split("\t");
+            team = parts[0];
+            wins = Integer.parseInt(parts[1]);
+            loss = Integer.parseInt(parts[2]);
+            pct = (double)wins/(wins+loss);
+            gb = ((leaderW - wins)+(loss - leaderL))/2;
+            if(gb == 0){
+                System.out.printf("%-20s %16d %8d %8.3f %7s\n", team, wins, loss, pct, "-");
+            }else{
+                System.out.printf("%-20s %16d %8d %8.3f %8.1f\n", team, wins, loss, pct, gb);  
+            }
+        }
+        System.out.println("\n");
+    }
+    
     public static void main(String[] args) {
         heading();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the standings filename: ");
         String fileName = sc.nextLine();
+
         String line;
-        String western[] = new String[9];
-        String eastern[] = new String[9];
-        int counter;
+        String[] western = new String[6];
+        String[] eastern = new String[6];
+        ArrayList<String> league = new ArrayList<String>();
+        int ctr;
+
         try{
-            Scanner fsc = new Scanner(new File(fileName));
+            Scanner fsc = new Scanner(new File(fileName));    
             while (fsc.hasNextLine()){
-                counter = 0;
                 line = fsc.nextLine();
-                if(line.equalsIgnoreCase("conference: western")){ 
-                    western[counter] = line;
-                }
-                
-                System.out.println(line);
-                counter++;
-            }
-            fsc.close();
-        } catch(Exception ex){
+                league.add(line);
+                //System.out.println(line);   
+            }    
+        } catch (Exception ex){
             System.out.println("A problem happened reading from the file");
         }  
+
+        for (int i =0; i < league.size(); i++){
+            if (league.get(i).equalsIgnoreCase("Conference: Eastern")){
+                ctr = 0;
+                for(int j = i; j < i+6; j++){
+                    eastern[ctr] = league.get(j+1);
+                    ctr++;
+                }
+            }else if (league.get(i).equalsIgnoreCase("Conference: Western")){
+                ctr = 0;
+                for(int j = i; j < i+6; j++){
+                    western[ctr] = league.get(j+1);
+                    ctr++;
+                }
+            }
+        }
+
         System.out.println("The teams have been read.");
         int choice = 0;
         while(choice != 4){
             menu();
             choice = sc.nextInt();
             if (choice == 1){
-                printResults();
+                System.out.println("\nStandings for the Eastern Conference");
+                printResults(eastern);
             } else if (choice == 2){
-                printResults();
+                System.out.println("\nStandings for the Western Conference");
+                printResults(western);
             } else if (choice == 3){
-                printResults();
+                System.out.println("Combined Conference Standings");
+                //printResults();
             }else{
                 break;
             }
         }
-        for(int j =0; j<9;j++){
-            System.out.println(western[j]);
+   
+        
+        //System.out.println(league);
+      
+
+        /*for (int num=0; num < eastern.length; num++){
+            System.out.println(eastern[num]);  
         }
+        for (int num=0; num < western.length; num++){
+            System.out.println(western[num]);
+        }*/
+
     }
 }
