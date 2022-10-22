57db5d00f9acf87b8009121565975ca6932ebc78
Grover Soans <soansg3445@gmail.com>
2022-10-21 20:15:46 -0500

    Author class commit

--- src/storyteller/Author.java
+++ src/storyteller/Author.java
@@ -1,5 +1,60 @@
 package storyteller;
 
+import java.util.Random;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
 public class Author {
+    private int adjFreq;
+    private int advFreq;
+    private int prepFreq;
+    WordFileReader f = new WordFileReader();
+    public static String getWord(f.dictionary){
+
+    }
+
+    
+
+    public int getAdjFreq() {
+        return adjFreq;
+    }
+    public int getAdvFreq() {
+        return advFreq;
+    }
+    public int getPrepFreq() {
+        return prepFreq;
+    }
+    public void setAdjFreq(int adjFreq) {
+        if (adjFreq < 0){
+            this.adjFreq = 0;
+        } else if (adjFreq > 10){
+            this.adjFreq = 10;
+        }else {
+            this.adjFreq = adjFreq;
+        }
+    }
+    public void setAdvFreq(int advFreq) {
+        if (advFreq < 0){
+            this.advFreq = 0;
+        } else if (adjFreq > 10){
+            this.advFreq = 10;
+        }else {
+            this.advFreq = advFreq;
+        }
+    }
+    public void setPrepFreq(int prepFreq) {
+        if (prepFreq < 0){
+            this.prepFreq = 0;
+        } else if (prepFreq > 10){
+            this.prepFreq = 10;
+        } else {
+            this.prepFreq = prepFreq;
+        }   
+    }
+    
+
+
+
+
     
 }
